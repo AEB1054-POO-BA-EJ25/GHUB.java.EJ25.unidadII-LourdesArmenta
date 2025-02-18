59465a7dcc75b6507074bc8083dc1a3c512f914a
armenta lindoro maria lourdes <137097961+mariaLourdesArmenta@users.noreply.github.com>
2025-02-18 15:04:04 +0000

    uso de Propiedades Get y Set

--- miPrincipal/Persona.java
+++ miPrincipal/Persona.java
@@ -17,8 +17,42 @@ public class Persona{
     }
 
     //Métodos Personalizados
-    public saludar(){
+    public void saludar(){
         System.out.println("Hola Mundo, soy una persona");
     }
+
+    //Definir Propiedades
+    //Lee el atributo nombre y lo regresa
+    public String getNombre(){
+        return this.nombre;
+    }
+    //Asignar un nombre al atributo nombre
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+
+    //Lee el atributo edad y lo regresa
+    public int getEdad(){
+        return this.edad;
+    }
+
+    //asigna el parametro edad al atributo edad
+    public void setEdad(int edad){
+        this.edad = edad;
+    }
+
+    //Lee el atributo DNI y lo regresa
+    public String getDni(){
+        return this.dni;
+    }
+
+    //asigna el parametro DNI al atributo DNI
+    public void setDni(String dni){
+        this.dni = dni;
+    }
+
+
+
 
 }
--- miPrincipal/Principal.java
+++ miPrincipal/Principal.java
@@ -4,6 +4,40 @@ public class Principal {
     
 
     public static void main(String[] args) {
+        //Crear una instancia de la clase Persona
+        Persona p1 = new Persona(); //invoco a su constructor vacio
+        Persona p2 = new Persona("Juan",18,"12345");
+
+        //Modificar el atributo de p1
+
+        p1.setNombre("Joaquin");
+        p1.setEdad(20);
+        p1.setDni("555");
+
+        //Leer al objeto p1
+
+        System.out.println("Nombre de p1="+p1.getNombre());
+        System.out.println("Edad de p1="+p1.getEdad());
+        System.out.println("DNI de p1="+p1.getDni());
+        
+
+        Alumno a1 = new Alumno(); //invoco al su constructor vacio
+        Alumno a2 = new Alumno("Alicia",20,"23456","Sistemas");
+
+        //invoco al método saludar de Persona
+        p1.saludar();
+        p2.saludar();
+
+
+        //invocar al método estudiar de Persona
+
+        a1.estudiar();
+        a2.estudiar();
+
+        a1.saludar();
+        a2.saludar();
+
+
        
     }
 }
