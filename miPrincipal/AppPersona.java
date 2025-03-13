759809ace8efed1ac21cd036d292310b0a9b74d3
armenta lindoro maria lourdes <137097961+mariaLourdesArmenta@users.noreply.github.com>
2025-03-13 14:59:14 +0000

    iniciando con la lista de personas

--- miPrincipal/AppPersona.java
+++ miPrincipal/AppPersona.java
@@ -1,10 +1,76 @@
 package miPrincipal;
 
+import java.util.Scanner;
+
 public class AppPersona {
     public static void menu(){
-        System.out.println("*******************");
-        System.out.println("   CLASE PERSONA");
-        System.out.println("*******************");
+        //crear un menu de opciones
+        Persona lista[] = new Persona[5]; //crea un arrego con capacidad para almacenar 5 personas
+
+        //indice para agredar personas
+        int i = 0;
+
+        Scanner teclado = new Scanner(System.in);
+        int opc = 0;
+        do{
+            System.out.println("*******************");
+            System.out.println("   CLASE PERSONA");
+            System.out.println("*******************");
+            System.out.println("1) Agregar");
+            System.out.println("2) Eliminar");
+            System.out.println("3) Modificar");
+            System.out.println("4) Consular");
+            System.out.println("\t 0 salir");
+
+            opc = teclado.nextInt();
+            switch (opc) {
+                case 1:
+                    teclado.nextLine(); //limpia el buffer del tecladomaria 
+                    //solitar dator por el teclado para crear un objeto persona
+                    System.out.print("Proporciona nombre:");
+                    String nombre = teclado.nextLine();
+
+                    System.out.print("Proporciona Edad:");
+                    int edad = teclado.nextInt();
+
+                    System.out.println("proporciona DNI:");
+                    String dni = teclado.next();
+
+                    Persona p1 = new Persona(nombre,edad,dni);
+
+                    lista[i]=p1;
+                    i++;
+
+
+
+                    break;
+                case 2:
+
+                    break;
+
+                case 3:
+
+                    break;
+                case 4:
+                    teclado.nextLine(); //limpio el buffer
+                    System.out.println("Dame la posicion de la persona que quieres consultar:");
+                    int pos = teclado.nextInt();
+                    Persona p2 = lista[pos];
+                    System.out.println("Nombre de la persona: "+p2.getNombre());
+                    System.out.println("Edad de la persona: "+p2.getEdad());
+                    System.out.println("DNI de la persona: "+p2.getDni());
+
+
+                    break;
+                case 0:
+
+                    return;
+
+                default:
+                    System.out.println("Opcion incorrecta, intente de nuevo");
+
+            }
+        } while (opc !=0);
         //crear una instancia de la clase persona
         Persona p1 = new Persona(); //invoco a su constructor vacio
         Persona p2 = new Persona("Juan",18,"12345");
